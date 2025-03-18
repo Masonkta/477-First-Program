2725b848272afc5a31f85eb580807362904a23a1
Christian-Hal <115608410+Christian-Hal@users.noreply.github.com>
2025-03-18 00:34:11 -0500

    Beginnings of enemies

--- My project/Assets/Scripts/Gameplay/EnemyDiver.cs
+++ My project/Assets/Scripts/Gameplay/EnemyDiver.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EnemyDiver : MonoBehaviour
 {
+    public float maxSpeed = 5f;
+    public float diveAccel = 1f;
+
+    public GameObject bulletPrefab;
+    public Transform spawnPoint;
+
+    public float randShootMin = .5f;
+    public float randShootMax = 3f;
+
+    public float restTime = 2f;
+    public float randMoveTimeMin = 1f;
+    public float randMoveTimeMax = 3f;
+
+    public float randDiveMin = 10f;
+    public float randDiveMax = 20f;
+
+    public Transform playerLocation;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print(Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // every y seconds move in a random direction up or down
+        // at some point randomly gravitate toward the player
     }
+
 }
