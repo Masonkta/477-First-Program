bb11b800631fe7df7613a55d0644285b0fc50950
Christian-Hal <115608410+Christian-Hal@users.noreply.github.com>
2025-03-23 18:05:18 -0500

    Enemy diver logic (collision detection needed)

--- My project/Assets/Scripts/Gameplay/EnemySpawn.cs
+++ My project/Assets/Scripts/Gameplay/EnemySpawn.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemySpawn : MonoBehaviour
 {
     public float spawnTimeMin = 3f;
     public float spawnTimeMax = 5f;
+    public float numSpawnedMax = 4;
+    public float numSpawnedMin = 1;
+    
+
     public GameObject enemy;
     public Transform spawnPoint;
+    private Transform spawnTarget;
 
     private float timer = 0f;
     private float randomFloat = 2;
+    private float numSpawned = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        spawnTarget = spawnPoint;
     }
 
     // Update is called once per frame
@@ -23,9 +31,14 @@ public class EnemySpawn : MonoBehaviour
         timer += Time.deltaTime;
         if(timer >= randomFloat)
         {
+
             timer = 0;
             randomFloat = Random.Range(spawnTimeMin, spawnTimeMax);
-            Instantiate(enemy, spawnPoint);
+            for (int i = 0; i < numSpawned; i++)
+            {
+                Instantiate(enemy, spawnTarget);
+            }
+            
         }
     }
 }
